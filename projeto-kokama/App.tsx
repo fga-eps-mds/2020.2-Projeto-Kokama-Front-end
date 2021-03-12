534d2ee30985db946892ffd68889d2a725e0ed8e
luisgaboardi <luis.ggaboardi@gmail.com>
2021-03-11 22:15:14 -0300

    Add choose language section
    Co-authored-by: aluzianobriceno <aluzianobriceno@gmail.com>

--- projeto-kokama/App.tsx
+++ projeto-kokama/App.tsx
@@ -23,6 +23,30 @@ export default function App() {
         <View style={styles.logoArea}>
           <Image style={styles.logo} source={require("./assets/logo.png")} />
         </View>
+
+        {/* Área de escolha da língua */}
+        <View style={styles.changeLanguage}>
+          {/* Primeiro idioma */}
+          <View style={styles.originLanguageArea}>
+            <Text style={styles.originLanguage}>{"originLanguage"}</Text>
+          </View>
+
+          {/* Ícone de mudança de idioma */}
+          <View style={styles.languageExchangeArea}>
+            <TouchableWithoutFeedback>
+              <Image
+                style={styles.languageExchange}
+                source={require("./assets/exchange.png")}
+              />
+            </TouchableWithoutFeedback>
+          </View>
+
+          {/* Segundo idioma */}
+          <View style={styles.destLanguageArea}>
+            <Text style={styles.destLanguage}>{"destLanguage"}</Text>
+          </View>
+        </View>
+
       </SafeAreaView>
     </ScrollView>
   );
@@ -49,5 +73,38 @@ const styles = StyleSheet.create({
     height: 80,
     width: 80,
     top: 15,
+  },
+  changeLanguage: {
+    height: 55,
+    flexDirection: "row",
+    borderBottomWidth: 1,
+    width: window.width,
+    backgroundColor: "#e5e5e5",
+  },
+  originLanguageArea: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  originLanguage: {
+    textAlign: "center",
+    fontSize: 22,
+  },
+  languageExchangeArea: {
+    flex: 0.7,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  languageExchange: {
+    width: 50,
+    height: 45,
+  },
+  destLanguageArea: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  destLanguage: {
+    textAlign: "center",
+    left: 8,
+    fontSize: 22,
   },
 });
