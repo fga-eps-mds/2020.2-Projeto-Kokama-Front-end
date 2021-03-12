26461d977e84ca48587a7972354411ca6ed94f7a
luisgaboardi <luis.ggaboardi@gmail.com>
2021-03-11 22:10:23 -0300

    Add header and logo within it
    Co-authored-by: aluzianobriceno <aluzianobriceno@gmail.com>

--- projeto-kokama/App.tsx
+++ projeto-kokama/App.tsx
@@ -1,14 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Dimensions,
+  Text,
+  TextInput,
+  StyleSheet,
+  Image,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+  ScrollView,
+} from "react-native";
+
+// Constantes e variáveis globais
+const window = Dimensions.get("window");
+const screen = Dimensions.get("screen");
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Hello world! Salve pro André</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.scrollBar}>
+      <SafeAreaView style={styles.container}>
+        {/* Área da logo */}
+        <View style={styles.logoArea}>
+          <Image style={styles.logo} source={require("./assets/logo.png")} />
+        </View>
+      </SafeAreaView>
+    </ScrollView>
   );
 }
 
@@ -18,5 +34,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  scrollBar: {},
+  logoArea: {
+    height: 100,
+    borderBottomWidth: 1,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    paddingBottom: 10,
+    backgroundColor: "#e5e5e5",
+  },
+  logo: {
+    height: 80,
+    width: 80,
+    top: 15,
   },
 });
