08d26b0fb448b990b5540014d785b91bfa0532a9
Luiz Gustavo <luiz1gustavocampos@gmail.com>
2021-04-16 17:50:25 -0300

    Fix some styles bugs

--- projeto-kokama/src/screens/About/index.tsx
+++ projeto-kokama/src/screens/About/index.tsx
@@ -7,7 +7,6 @@ import {
 } from "react-native";
 import React from "react";
 import aboutStyle from "./styles";
-import {AboutImages} from "../../components/AboutImages"
 
 export default function About({ navigation }) {
     return (
@@ -15,75 +14,75 @@ export default function About({ navigation }) {
             <ScrollView>
                 <View style={aboutStyle.container}>
                     
-                    <Text style={aboutStyle.texts}>Equipe de Gerência-UnB:</Text>
+                    <Text style={aboutStyle.title}>Equipe de Gerência-UnB:</Text>
                     <View style={aboutStyle.EPS}>
                         <View style={aboutStyle.photo}>
                             <Image source={require('../../assets/img/team-UnB/Andre.png')}
                                 style={aboutStyle.photos}
                             />
-                            <Text>Andre Lucas</Text>
+                            <Text style={aboutStyle.text}>Andre Lucas</Text>
                         </View>
                         <View style={aboutStyle.photo}>
                             <Image source={require('../../assets/img/team-UnB/Leonardo.png')}
                                 style={aboutStyle.photos}
                             />
-                            <Text>Leonardo Medeiros</Text>
+                            <Text style={aboutStyle.text}>Leonardo Medeiros</Text>
                         </View>
                         <View style={aboutStyle.photo}>
                             <Image source={require('../../assets/img/team-UnB/Lieverton.png')}
                                 style={aboutStyle.photos}
                             />
-                            <Text>Lieverton Santos</Text>
+                            <Text style={aboutStyle.text}>Lieverton Santos</Text>
                         </View>
                         <View style={aboutStyle.photo}>
                             <Image source={require('../../assets/img/team-UnB/Welison.jpeg')}
                                 style={aboutStyle.photos}
                             />
-                            <Text>Welison Almeida</Text>
+                            <Text style={aboutStyle.text}>Welison Almeida</Text>
                         </View>
                     </View>
 
-                    <Text style={aboutStyle.texts}>Equipe de Desenvolvimento UnB:</Text>
+                    <Text style={aboutStyle.title}>Equipe de Desenvolvimento UnB:</Text>
                     <View style={aboutStyle.MDS}>
                         <View style={aboutStyle.photo}>
                             <Image source={require('../../assets/img/team-UnB/Ana.png')}
                                 style={aboutStyle.photos}
                             />
-                            <Text>Ana Júlia</Text>
+                            <Text style={aboutStyle.text}>Ana Júlia</Text>
                         </View>
                         <View style={aboutStyle.photo}>
                             <Image source={require('../../assets/img/team-UnB/Fernando.png')}
                                 style={aboutStyle.photos}
                             />
-                            <Text>Fernando Vargas</Text>
+                            <Text style={aboutStyle.text}>Fernando Vargas</Text>
                         </View>
                         <View style={aboutStyle.photo}>
                             <Image source={require('../../assets/img/team-UnB/Lais.png')}
                                 style={aboutStyle.photos}
                             />
-                            <Text>Lais Portela</Text>
+                            <Text style={aboutStyle.text}>Lais Portela</Text>
                         </View>
                         <View style={aboutStyle.photo}>
                             <Image source={require('../../assets/img/team-UnB/Lucas.png')}
                                 style={aboutStyle.photos}
                             />
-                            <Text>Lucas Rodrigues</Text>
+                            <Text style={aboutStyle.text}>Lucas Rodrigues</Text>
                         </View>
                         <View style={aboutStyle.photo}>
                             <Image source={require('../../assets/img/team-UnB/Luis.png')}
                                 style={aboutStyle.photos}
                             />
-                            <Text>Luís Guilherme</Text>
+                            <Text style={aboutStyle.text}>Luís Guilherme</Text>
                         </View>
                         <View style={aboutStyle.photo}>
                             <Image source={require('../../assets/img/team-UnB/Luiz.png')}
                                 style={aboutStyle.photos}
                             />
-                            <Text>Luiz Gustavo</Text>
+                            <Text style={aboutStyle.text}>Luiz Gustavo</Text>
                         </View>
                     </View>
 
-                    <Text style={aboutStyle.texts}>Apoiadores</Text>
+                    <Text style={aboutStyle.title}>Apoiadores</Text>
                     <View style={aboutStyle.EPS}>
                         <Image source={require('../../assets/img/UnB.jpg')} 
                             style={aboutStyle.UnB}
