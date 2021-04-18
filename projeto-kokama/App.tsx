37001062ccbdd532cc59b8e870d8cb90791acf8e
Luiz Gustavo <luiz1gustavocampos@gmail.com>
2021-04-18 17:02:28 -0300

    Change some about screen styles and show

--- projeto-kokama/App.tsx
+++ projeto-kokama/App.tsx
@@ -43,7 +43,7 @@ export default function App() {
         <Tab.Screen name="Usuário" component={UserStackScreen}
           options={{
             tabBarIcon: props => (
-              <IconA name="exclamation-circle" size={30} color={props.color}/>
+              <Icon5 name="info-circle" size={30} color={props.color}/>
             )
           }}
         />
--- projeto-kokama/src/screens/About/styles.ts
+++ projeto-kokama/src/screens/About/styles.ts
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
     },
     EPS: {
         display: "flex",
-        maxHeight: 250,
+        maxHeight: 130,
         justifyContent: "space-around",
         flexDirection: "row",
         flexWrap: "wrap",
@@ -31,7 +31,7 @@ const styles = StyleSheet.create({
     },
     MDS: {
         display: "flex",
-        maxHeight: 250,
+        maxHeight: 210,
         justifyContent: "space-around",
         flexDirection: "row",
         flexWrap: "wrap",
@@ -42,13 +42,20 @@ const styles = StyleSheet.create({
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-        borderTopWidth: 15,
-        borderColor: Colors.LIGHT_GRAY,
     },
     photos: {
         maxHeight: 80,
         maxWidth: 80,
         borderRadius: 250,
+    },
+    apoiadores: {
+        display: "flex",
+        maxHeight: 250,
+        justifyContent: "space-around",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        borderWidth: 10,
+        borderColor: Colors.LIGHT_GRAY,
     },
     UnB: {
         display: "flex",
