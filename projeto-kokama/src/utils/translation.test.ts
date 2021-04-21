3afbcbde5cc9f6ffb659fd252b7846ae43f74971
Fernando Vargas <nandovargas7@gmail.com>
2021-04-20 21:20:30 -0300

    test the utils functions of translations and adding Jest
    Co-authored-by: laispa <lais3722@gmail.com>

--- projeto-kokama/src/utils/translation.test.ts
+++ projeto-kokama/src/utils/translation.test.ts
@@ -1,8 +1,15 @@
-import { capitalizeFirstLetter } from "./translation";
+import { capitalizeFirstLetter, removeStringFromMarkers } from "./translation";
 
 test("Check if first letter is capitalized", () => {
   const capitalized = capitalizeFirstLetter("kokama KinKin");
   const expected = "Kokama KinKin";
 
   expect(capitalized).toBe(expected);
+});
+
+test("Check if we get the string between the tags", () => {
+  const word = removeStringFromMarkers("eu tenho uma >Palavra<", ">", "<");
+  const expected = "Palavra";
+
+  expect(word).toBe(expected);
 });
