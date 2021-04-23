5e268588bd5c153bb5fbc6f2782739e7ad533262
Fernando Vargas <nandovargas7@gmail.com>
2021-04-22 22:32:49 -0300

    test in component History
    Co-authored-by: laispa <lais3722@gmail.com>

---
+++ projeto-kokama/src/components/components.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import History from './History';
+import { HistoryTuple } from "../screens/Translation/interface";
+
+
+interface Props {
+    isEnabled: boolean;
+    data: HistoryTuple[];
+    onPressTitle: () => void;
+    translateFrom: string;
+    onPressWord: (kokamaWord:string, originLanguage:string) => void;
+}
+
+let props:Props
+beforeEach(() => {
+    props = {
+        isEnabled: true,
+        data: [{
+            kokama: "pacha",
+            portuguese: "cem",
+          }],
+        onPressTitle: () => {},
+        translateFrom: "portugues",
+        onPressWord:() =>{},
+    };
+  });
+
+test('renders correctly', () => {
+  const tree = renderer.create(<History {... props}/>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
--- projeto-kokama/src/screens/Translation/test.test.tsx
+++ projeto-kokama/src/screens/Translation/test.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Translation from '../Translation/index';
 
+
 test('renders correctly', () => {
   const tree = renderer.create(<Translation />).toJSON();
   expect(tree).toMatchSnapshot();
