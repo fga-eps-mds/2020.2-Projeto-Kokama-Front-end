edd6af289d3181b2e3b474edb6c45b6463009117
Fernando Vargas <nandovargas7@gmail.com>
2021-04-27 02:32:36 -0300

    Resolve async test in translation screen
    Co-authored-by: laispa <lais3722@gmail.com>

--- projeto-kokama/src/screens/Translation/test.test.tsx
+++ projeto-kokama/src/screens/Translation/test.test.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import Translation from '../Translation/index';
+import Translation from './index';
 import {act, create}  from 'react-test-renderer';
 
 const tree = create(<Translation />);
 
-test('renders correctly', () => {
-  const TouchableWithoutFeedback = tree.root.findByProps({testID:'insertSymbol'}).props;
-  act(() => TouchableWithoutFeedback.onPress());
+function sleep(ms:number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
+test('renders correctly', async () => {
+  const TouchableWithoutFeedback = tree.root.findByProps({testID:'insertSymbol'}).props;
+  await act(()=> sleep(500) as unknown as void);
+  act (() => TouchableWithoutFeedback.onPress());
   expect(tree).toMatchSnapshot();
 });
 
-test('change language', () =>{
+
+test('change language', () => {
   const TouchableWithoutFeedback = tree.root.findByProps({testID:'exchangeLanguage'}).props;
   act(() => TouchableWithoutFeedback.onPress());
 });
+
