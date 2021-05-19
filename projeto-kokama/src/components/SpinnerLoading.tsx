c8e3e46a97141e517bb781e663ca5d28e80943c1
luisgaboardi <luis.ggaboardi@gmail.com>
2021-05-19 19:24:53 -0300

    Remove unnused import
    Co-authored-by: Ana Júlia <aluzianobriceno@gmail.com>

--- projeto-kokama/src/components/SpinnerLoading.tsx
+++ projeto-kokama/src/components/SpinnerLoading.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StyleSheet, View, Text, } from "react-native";
 import { Spinner } from "native-base";
 import Colors from "../assets/Colors"
