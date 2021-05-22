35a7485b262b9a79710b319bc59e710ff5a098e7
luisgaboardi <luis.ggaboardi@gmail.com>
2021-05-22 13:49:16 -0300

    Exchange string for constant in test

--- projeto-kokama/src/__tests__/utils/activity.test.ts
+++ projeto-kokama/src/__tests__/utils/activity.test.ts
@@ -1,3 +1,4 @@
+import { KOKAMA, PORTUGUESE } from "../../config/constants";
 import { removeMarkers, createBlankSpace } from "../../utils/activity";
 
 test("Check if markers where removed", () => {
@@ -8,15 +9,15 @@ test("Check if markers where removed", () => {
 });
 
 test("Check if word was replaced by blank space", () => {
-    let result = createBlankSpace("testando <123>", "123", "portuguese");
+    let result = createBlankSpace("testando <123>", "123", PORTUGUESE);
     let expected = "testando 123";
     expect(result).toBe(expected);
 
-    result = createBlankSpace("testando <123>", "123", "kokama");
+    result = createBlankSpace("testando <123>", "123", KOKAMA);
     expected = "testando _______";
     expect(result).toBe(expected);
 
-    result = createBlankSpace("testando <123>", "456", "kokama");
+    result = createBlankSpace("testando <123>", "456", KOKAMA);
     expected = "Error 404: Frase não encontrada";
     expect(result).toBe(expected);
 });
